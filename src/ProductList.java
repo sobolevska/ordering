164f40d5fbebfbe6979a25d8fac9588b1be500b8
Toms Kusiņš <t.kusins@gmail.com>
2019-04-15 18:43:47 +0300

    ProductList impl

---
+++ src/ProductList.java
@@ -0,0 +1,15 @@
+public class ProductList {
+    private Product[] products;
+
+    public ProductList() {
+
+    }
+
+    public void printProducts() {
+
+    }
+
+    private void parseFile() {
+
+    }
+}
