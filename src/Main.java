9bbfb6ed97185c6d3944eb37f54be49c10e8f9ff
Renata Svikle <renata.svikle@inbox.lv>
2019-06-06 19:00:50 +0300

    Added comment from Renata

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         ProductList list = new ProductList();
         list.printProducts();
-        
+
+
+        //pievienots komentārs Jeļenas projektam
     }
 }
