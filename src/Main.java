b9edaaed14648389742938a14af7c5dd3edf46a8
Jeļena Soboļevska <jelena1984@gmail.com>
2019-06-06 18:32:45 +0300

    comment

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
         ProductList list = new ProductList();
         list.printProducts();
+
+        System.out.println("product list");
     }
 }
